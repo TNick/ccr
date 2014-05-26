982f850312641678f8ff3aba649608ad400bf06e
Nicu Tofan <nicu.tofan@gmail.com>
2014-05-26 13:04:43 +0300

    ready to extract values from db

--- php/data.php
+++ php/data.php
@@ -14,6 +14,7 @@ function debugvar($foo)
 {
     debugmsg(var_export($foo, true));
 }
+// reports the error as json and terminates the execution
 function returnError($foo)
 {
     $responses = array();
@@ -27,16 +28,18 @@ function getRealNumbers($str)
     return $matches[0];
 }
 
-
-
-// read JSon input
+// read JSon input; JSon request format is :
+// {"database":"ccr",
+//  "scale_categ":"2",
+//  "view":{
+//    "left":"10", 
+//    "top":"10", 
+//    "right":"100", 
+//    "bottom":"100"}
+// }
 $raw_data = file_get_contents('php://input');
 $data_back = json_decode($raw_data);
-// JSon request format is :
-// {"database":"ccr","scale_categ":"2","view":{"left":"10", "top":"10", "right":"100", "bottom":"100"}}
-
 debugvar($data_back);
-//debugvar($_ENV);
 
 // set json string to php variables
 $database = 'ccr';
@@ -62,28 +65,44 @@ if (array_key_exists("view", $data_back)) {
     returnError('A bounding box was not provided');
 }
 
-// Connecting, selecting database
+// make sure input is numeric
+$d_left = 0.0;
+$d_top = 0.0;
+$d_right = 0.0;
+$d_bottom = 0.0;
+$i_scale_categ = 1;
+try {
+	// make sure values are numbers
+	$d_left = floatval($view->{'left'});
+	$d_top = floatval($view->{'top'});
+	$d_right = floatval($view->{'right'});
+	$d_bottom = floatval($view->{'bottom'});
+	$i_scale_categ = intval($scale_categ);
+} catch(Exception $ex) {
+	debugvar($view);
+	debugvar($ex);
+	returnError(json_encode("Input data not properly encoded: " . $ex->getMessage()));
+}
 
-// OPENSHIFT specific
+// get database specific info
 $host = getenv('OPENSHIFT_MYSQL_DB_HOST');
-$port = getenv('OPENSHIFT_MYSQL_DB_PORT');
-$user = '';
-$pass = '';
-if ($host) {
+$port = NULL;
+$user = NULL;
+$pass = NULL;
+if ($host) { // OPENSHIFT specific
+	$port = getenv('OPENSHIFT_MYSQL_DB_PORT');
     if ($port) {
         $host = $host . ":" . $port;
     }
     $user = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
     $pass = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
-    //$link = mysql_connect($host, $user, $pass)
-    //    or returnError('Could not connect: ' . mysql_error());
     $database = getenv('OPENSHIFT_APP_NAME');
 } else {
     returnError(json_encode("Unknown environment"));
 }
 
-
-$pdo = NULL
+// attempt a connection with provided information
+$pdo = NULL;
 try {
 	$pdo = new PDO ('mysql:host=' . $host . ';dbname=' . $database . ';charset=utf8', $user, $pass);
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -94,43 +113,13 @@ try {
 }
 debugmsg('Connected successfully');
 
-
-// mysql_select_db($database) or returnError('Could not select database');
-
-// we define a polygone (a rectangle) and ask for all records that intersect that rectangle
-// and are part of specified layer
-//$query_base = "SELECT kind,AsText(bbox),assoc_data FROM spatialdata WHERE layer=%5$d AND Intersects( bbox, PolyFromText( 'POLYGON((%1$f %2$f,%3$f %2$f,%3$f %4$f,%1$f %4$f))' ) ) LIMIT 10000000;";
-//debugvar($query_base);
-//$query = sprintf($query_base, $view->{"left"}, $view->{"top"}, $view->{"right"}, $view->{"bottom"}, $scale_categ);
-//debugvar($query);
-//$responses = mysql_query($query) or returnError('Query failed: ' . mysql_error());
-
-// create json response
+// we accumulate results in this array
 $responses = array();
 
-$d_left = 0.0;
-$d_top = 0.0;
-$d_right = 0.0;
-$d_bottom = 0.0;
-$i_scale_categ = 1;
-
+// execute the querry and export in our array
 try {
-	try {
-		// make sure values are numbers
-		$d_left = floatval($view->{'left'});
-		$d_top = floatval($view->{'top'});
-		$d_right = floatval($view->{'right'});
-		$d_bottom = floatval($view->{'bottom'});
-		$i_scale_categ = intval($scale_categ);
-	} catch(Exception $ex) {
-		debugvar($view);
-		debugvar($ex);
-		returnError(json_encode("Input data not properly encoded: " . $ex->getMessage()));
-	}
-	
 	$query_base = "SELECT kind,AsText(bbox),assoc_data FROM spatialdata WHERE layer=".$i_scale_categ." AND Intersects( bbox, PolyFromText( 'POLYGON((".$d_left." ".$d_top.",".$d_right." ".$d_top.",".$d_right." ".$d_bottom.",".$d_left." ".$d_bottom."))' ) ) LIMIT 10000000;";
-	
-	$myPDO = $pdo->query($query_base))
+	$myPDO = $pdo->query($query_base);
 	while($row = $myPDO->fetch(PDO::FETCH_ASSOC)) {
 		debugmsg($row["kind"]);
 		debugmsg($row["bbox"]);
@@ -141,8 +130,6 @@ try {
 		$proper_box = array($coords[0], $coords[1], $coords[2], $coords[5]);
 		$responses[] = array_merge(array("kind" => $row["kind"], "box" => $proper_box ), $assoc_data);
 	}
-
-    
 } catch(PDOException $ex) {
 	debugvar($ex);
 	returnError(json_encode("Failed to query database: " . $ex->getMessage()));
@@ -151,41 +138,7 @@ try {
 
 
 
-
-//while ($row = mysql_fetch_assoc($responses, MYSQL_ASSOC)) {
-    //debugmsg($row["kind"]);
-    //debugmsg($row["bbox"]);
-    //debugmsg($row["assoc_data"]);
-
-    //$assoc_data = unserialize($row["assoc_data"]); // $row["assoc_data"]) = serialize($assoc_data);
-    //$coords = getRealNumbers($row["bbox"]);
-    //$proper_box = array($coords[0], $coords[1], $coords[2], $coords[5]);
-    //$responses[] = array_merge(array("kind" => $row["kind"], "box" => $proper_box ), $assoc_data);
-//}
-
-// Free resultset
-// mysql_free_result($responses);
-
-// Closing connection
-// mysql_close($link);
-
-
-
-// CREATE TABLE IF NOT EXISTS 'spatialdata' (
-//   'id' int(10) unsigned NOT NULL AUTO_INCREMENT,
-//   'kind' enum('image','vector','html','text') NOT NULL,
-//   'layer' int(10) unsigned NOT NULL,
-//   'bbox' polygon NOT NULL,
-//   'assoc_data' text NOT NULL,
-//   PRIMARY KEY ('id'),
-//   SPATIAL KEY 'bbox' ('bbox'),
-//   KEY 'layer' ('layer')
-// ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='Spatial data for Cosmic Calendar Reloaded application';
-// INSERT INTO 'ccr'.'spatialdata' ('id', 'kind', 'layer', 'assoc_data', 'bbox')
-//   VALUES (NULL, 'image', '1', '1', '2', '2', '1', '545', PolyFromText('POLYGON((0.123 0.123,10.123 0.123,10.123 10.123,0.123 10.123,0.123 0.123))'));
-// SELECT * FROM spatialdata WHERE layer=12 AND Intersects( bbox, PolyFromText( 'POLYGON((0.123 0.123,10.123 0.123,10.123 10.123,0.123 10.123,0.123 0.123))' ) );
-
-
+// some debug values to ensure non-empty
 $responses[] = array("kind" => "image", "box" => array(10, 10, 50, 50), "url" => "https://www.gravatar.com/avatar/" . "2a32c3039116405612b802f639557ffb?s=32&d=identicon&r=PG");
 $responses[] = array("kind" => "vector", "box" => array(20, 10, 50, 50), "vert" => array(20.0,20.2, 3.5,4.4, 100,10), "color" => "blue", "fill" => "transparent", "closed" => "true", "line_width" => "1" );
 $responses[] = array("kind" => "vector", "box" => array(200, 10, 50, 50), "vert" => array(200.0,200.2, 250.5,200.4, 250,250, 200,250), "color" => "blue", "fill" => "red", "closed" => "false");
